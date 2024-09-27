beec21043529c4570952365641a96606e1871881
viczor <qhvictor@gmail.com>
2024-09-27 01:35:06 -0500

    +

--- src/pages/PageA.jsx
+++ src/pages/PageA.jsx
@@ -1,10 +1,15 @@
-//PageA.jsx
-
 import './PageA.css';
 import logo from '../assets/contadorlogo.svg';
 import RegistrationForm from '../components/RegistrationForm.jsx';
+import { useNavigate } from 'react-router-dom';
 
 const PageA = () => {
+    const navigate = useNavigate();
+
+    const handleCornerClick = () => {
+        navigate('/pageb');
+    };
+
     return (
         <div className="page-container">
             <div className="content">
@@ -14,9 +19,14 @@ const PageA = () => {
                         Comprometidos con la formación y el desarrollo profesional, ofrecemos servicios innovadores y de alta calidad diseñados para profesionales que buscan la excelencia en sus carreras. Proveemos soluciones que empoderan a nuestros miembros con las herramientas necesarias para enfrentar los desafíos del mundo financiero actual.
                     </p>
                 </div>
-                <img src={logo} className="logo" alt="logo" />
+                <img src={logo} className="logo" alt="Logo de la organización" />
                 <div className="form-section">
                     <RegistrationForm />
+                </div>
+            </div>
+            <div className="corner" onClick={handleCornerClick}>
+                <div className="corner-content">
+                    <span>Ver más</span>
                 </div>
             </div>
         </div>
